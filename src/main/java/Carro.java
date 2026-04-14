92541b2a67d2d6014fe7e0fd868f160d12c771f0
RENAN LUCAS SOUZA DA SILVA <renan.lucas.119@ad.ufrn.br>
2026-04-14 16:25:00 -0300

    Salvando 14/04

---
+++ src/main/java/Carro.java
@@ -0,0 +1,67 @@
+public class Carro {
+    private String marca, modelo;
+    private int ano;
+    private double velocidadeAtual;
+
+    public String getMarca(){
+        return marca;
+
+    }
+
+    public void setMarca(String marca){
+        this.marca = marca;
+
+    }
+
+    public String getModelo(){
+        return modelo;
+
+    }
+
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+
+    }
+
+    public int getAno(){
+        return ano;
+
+    }
+
+    public void setAno(int ano){
+        this.ano = ano;
+
+    }
+
+    public double getVelocidadeAtual(){
+        return velocidadeAtual;
+
+    }
+
+    public void setVelocidadeAtual(double velocidadeAtual){
+        this.velocidadeAtual = velocidadeAtual;
+
+    }
+
+    public void acelerar(double incremento){
+        this.velocidadeAtual += incremento;
+
+    }
+
+    public void exibirVelocidade(){
+        System.out.println(getVelocidadeAtual() + " km");
+
+    }
+
+    public void frear(double decremento){
+        if(getVelocidadeAtual() < 0){
+            this.velocidadeAtual = 0;
+
+        }else{
+            this.velocidadeAtual -= decremento;
+
+        }
+
+    }
+
+}
